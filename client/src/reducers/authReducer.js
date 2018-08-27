6349210573ad30ed6940346d1781ad566e0d4c53
Carlos González <carlosantoniogonzalez@outlook.com>
2018-08-26 21:47:39 -0400

    fixed layer 8 error

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,13 @@
 import { FETCH_USER } from "../actions/types";
 
 export default function(state = null, action) {
-  console.log(action);
+    console.log(action);
   switch (action.type) {
-    case FETCH_USER:
+    case FETCH_USER: {
+      console.log(action);
       return action.payload || false;
+    }
+
     default:
       return state;
   }
